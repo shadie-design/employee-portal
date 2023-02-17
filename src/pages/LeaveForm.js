4a02aaa11313ed4db986e85d5f6d6c2146cd653f
Shadrack Githua <githuashadrack89@gmail.com>
2023-02-18 01:06:11 +0300

    Update form column size

--- src/pages/LeaveForm.js
+++ src/pages/LeaveForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import {Grid,Dialog, DialogActions, DialogContent,  DialogTitle, Typography } from '@material-ui/core';
+import { Grid, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@material-ui/core';
 import Controls from "../components/controls/Controls";
 import { IconButton } from '@material-ui/core';
 import { Close } from '@material-ui/icons';
@@ -35,18 +35,18 @@ const useStyles = makeStyles(theme => ({
 
 
 export default function LeaveForm(props) {
-    const { addOrEdit,LoadLeaveRequests,openPopup, recordForEdit,setOpenPopup } = props
+    const { addOrEdit, LoadLeaveRequests, openPopup, recordForEdit, setOpenPopup } = props
     const classes = useStyles();
-   
+
     const initialFValues = {
         id: 0,
         leaveTypeName: '',
         startDate: new Date(),
         relieverId: '',
-        leaveDuration  : 0,
-        leaveEnd : '',
+        leaveDuration: 0,
+        leaveEnd: '',
         leaveBalance: '',
-        resumptionDate : '',
+        resumptionDate: '',
     }
 
     const validate = (fieldValues = values) => {
@@ -70,10 +70,10 @@ export default function LeaveForm(props) {
     const { addToast } = useToasts()
     const [LeaveTypeData, setLeaveTypeData] = useState([]);
     const [leaveTypeName, setLeaveTypeName] = useState(null);
-    
- 
 
-   
+
+
+
 
     const {
         values,
@@ -84,11 +84,11 @@ export default function LeaveForm(props) {
         resetForm
     } = useForm(initialFValues, true, validate);
 
-// Load LeaveTypes
+    // Load LeaveTypes
 
-useEffect(() => {
-    GetEmployeeLeaveTypes();
-  }, []);
+    useEffect(() => {
+        GetEmployeeLeaveTypes();
+    }, []);
 
 
     const GetEmployeeLeaveTypes = () => {
@@ -96,137 +96,137 @@ useEffect(() => {
         const userToken = GetCookie("Token");
         const employeeLeaveRequestsUrl = Api.AppBaseUrl + 'EmployeePortal/Employee/GetEmployeeLeaveTypeNames/' + employeeEmail;
         fetch(employeeLeaveRequestsUrl, {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${userToken}`,
-          },
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userToken}`,
+            },
         })
-          .then(res => res.json())
-          .then((leaveTypeList) => {
-          //  console.log(leaveTypeList);
-            const EmployeeleaveTypeList = leaveTypeList;
-            setLeaveTypeData(EmployeeleaveTypeList);
-          })
-      }
-    
+            .then(res => res.json())
+            .then((leaveTypeList) => {
+                //  console.log(leaveTypeList);
+                const EmployeeleaveTypeList = leaveTypeList;
+                setLeaveTypeData(EmployeeleaveTypeList);
+            })
+    }
+
 
 
 
     const someDate = new Date(values.startDate);
     const numberOfDaysToAdd = Number(values.leaveDuration);
     const result = someDate.setDate(someDate.getDate() + (numberOfDaysToAdd));
-    
 
 
-const createNewLeaveRequest =()=>{
-    const CreateLeaveRequestUrl = Api.AppBaseUrl + 'EmployeePortal/Employee/CreateLeaveRequest';
-    const CustomStartDate = new Date(values.startDate)
-//ADD new leaveRequest
-const LeaveDuration = values.leaveDuration
-const StartDate = CustomStartDate
-const EndDate = new Date(result)
-const LeaveTypeName = leaveTypeName
-const EmployeeEmail = GetCookie("Email");
-const userToken = GetCookie("Token");
 
-if(LeaveDuration === null || LeaveTypeName === null || StartDate === null || LeaveDuration < 0.5 ){
+    const createNewLeaveRequest = () => {
+        const CreateLeaveRequestUrl = Api.AppBaseUrl + 'EmployeePortal/Employee/CreateLeaveRequest';
+        const CustomStartDate = new Date(values.startDate)
+        //ADD new leaveRequest
+        const LeaveDuration = values.leaveDuration
+        const StartDate = CustomStartDate
+        const EndDate = new Date(result)
+        const LeaveTypeName = leaveTypeName
+        const EmployeeEmail = GetCookie("Email");
+        const userToken = GetCookie("Token");
 
-}else{
-const requiredFields = {EmployeeEmail,LeaveTypeName, LeaveDuration, StartDate, EndDate}
-    try {
-        fetch(CreateLeaveRequestUrl, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${userToken}`,
-            },
-            body: JSON.stringify(requiredFields)
-        }).then(res => res.json())
+        if (LeaveDuration === null || LeaveTypeName === null || StartDate === null || LeaveDuration < 0.5) {
 
-            .then((responseCode) => {
-                console.log(responseCode)
-                if(responseCode.success == true){
-                    addToast(responseCode.message, { appearance: 'success' }); 
-                    setOpenPopup(false)    
-                    resetForm()
-                }else{
-                    addToast(responseCode.errorMessage, { appearance: 'error' }); 
-                    setOpenPopup(false)    
-                }
-                 LoadLeaveRequests()
-            })
+        } else {
+            const requiredFields = { EmployeeEmail, LeaveTypeName, LeaveDuration, StartDate, EndDate }
+            try {
+                fetch(CreateLeaveRequestUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${userToken}`,
+                    },
+                    body: JSON.stringify(requiredFields)
+                }).then(res => res.json())
 
-    } catch (e) {
+                    .then((responseCode) => {
+                        console.log(responseCode)
+                        if (responseCode.success == true) {
+                            addToast(responseCode.message, { appearance: 'success' });
+                            setOpenPopup(false)
+                            resetForm()
+                        } else {
+                            addToast(responseCode.errorMessage, { appearance: 'error' });
+                            setOpenPopup(false)
+                        }
+                        LoadLeaveRequests()
+                    })
 
+            } catch (e) {
+
+            }
+        }
     }
-}
-}
 
-const updateLeaveRequest =()=>{
-    const CreateLeaveRequestUrl = Api.AppBaseUrl + 'EmployeePortal/Employee/UpdateEmployeeLeaveRequest';
-    const CustomStartDate = new Date(values.startDate)
-//ADD new leaveRequest
-const Id = recordForEdit.id
-const LeaveDuration = values.leaveDuration
-const StartDate = CustomStartDate
-const EndDate = new Date(result)
-const LeaveTypeName = recordForEdit.leaveTypeName
-const EmployeeEmail = GetCookie("Email");
-const userToken = GetCookie("Token");
-const requiredFields = {Id,EmployeeEmail,LeaveTypeName, LeaveDuration, StartDate, EndDate}
-if(Id == null || LeaveDuration === null || LeaveTypeName === null || StartDate === null || LeaveDuration < 0.5 ){
-console.log(requiredFields);
-}else{
+    const updateLeaveRequest = () => {
+        const CreateLeaveRequestUrl = Api.AppBaseUrl + 'EmployeePortal/Employee/UpdateEmployeeLeaveRequest';
+        const CustomStartDate = new Date(values.startDate)
+        //ADD new leaveRequest
+        const Id = recordForEdit.id
+        const LeaveDuration = values.leaveDuration
+        const StartDate = CustomStartDate
+        const EndDate = new Date(result)
+        const LeaveTypeName = recordForEdit.leaveTypeName
+        const EmployeeEmail = GetCookie("Email");
+        const userToken = GetCookie("Token");
+        const requiredFields = { Id, EmployeeEmail, LeaveTypeName, LeaveDuration, StartDate, EndDate }
+        if (Id == null || LeaveDuration === null || LeaveTypeName === null || StartDate === null || LeaveDuration < 0.5) {
+            console.log(requiredFields);
+        } else {
 
-    try {
-        fetch(CreateLeaveRequestUrl, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${userToken}`,
-            },
-            body: JSON.stringify(requiredFields)
-        }).then(res => res.json())
+            try {
+                fetch(CreateLeaveRequestUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${userToken}`,
+                    },
+                    body: JSON.stringify(requiredFields)
+                }).then(res => res.json())
 
-            .then((responseCode) => {
-                console.log(responseCode)
-                if(responseCode.success == true){
-                    addToast(responseCode.message, { appearance: 'success' }); 
-                    setOpenPopup(false)    
-                    resetForm()
-                }else{
-                    addToast(responseCode.errorMessage, { appearance: 'error' }); 
-                    setOpenPopup(false)    
-                }
-                 LoadLeaveRequests()
-            })
+                    .then((responseCode) => {
+                        console.log(responseCode)
+                        if (responseCode.success == true) {
+                            addToast(responseCode.message, { appearance: 'success' });
+                            setOpenPopup(false)
+                            resetForm()
+                        } else {
+                            addToast(responseCode.errorMessage, { appearance: 'error' });
+                            setOpenPopup(false)
+                        }
+                        LoadLeaveRequests()
+                    })
 
-    } catch (e) {
+            } catch (e) {
 
+            }
+        }
     }
-}
-}
 
 
-   const handleClose=()=>{
-       setOpenPopup(false)
-       resetForm()
-   }
+    const handleClose = () => {
+        setOpenPopup(false)
+        resetForm()
+    }
 
     const handleSubmit = e => {
         e.preventDefault()
         createNewLeaveRequest()
         if (validate()) {
-         //   createNewLeaveRequest()
+            //   createNewLeaveRequest()
         }
-      
+
     }
 
-   
+
 
     useEffect(() => {
         if (recordForEdit != null)
@@ -236,36 +236,37 @@ console.log(requiredFields);
     }, [recordForEdit])
 
     return (
-        
+
         <Dialog open={openPopup} elevation={3} className={classes.root}>
-                <DialogTitle>
-                    <div className="flex justify-between items-center px-0 ">
+            <DialogTitle>
+                <div className="flex justify-between items-center px-0 ">
 
-                        Raise New Leave Request
-                        <IconButton
-                            aria-label="close"
-                            onClick={()=>{handleClose()}}
-                            sx={{
-                                position: 'absolute',
-                                right: 8,
-                                top: 8,
-                                color: (theme) => theme.palette.grey[500],
-                            }}
-                        >
-                            <Close />
-                        </IconButton>
+                    Raise New Leave Request
+                    <IconButton
+                        aria-label="close"
+                        onClick={() => { handleClose() }}
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                    >
+                        <Close />
+                    </IconButton>
 
-                    </div>
+                </div>
 
-                </DialogTitle>
+            </DialogTitle>
 
-                <DialogContent dividers>
-                    <div className="flex justify-between items-center px-0 ">
-                        <Grid container>
+            <DialogContent dividers>
+                <div className="flex justify-between items-center px-0 ">
 
-                            <Form onSubmit={handleSubmit}>
-                                <Grid container>
-                                    <Grid item xs={6}>                                     
+
+                        <Form onSubmit={handleSubmit}>
+                            <Grid container mb={12}>
+                                <Grid item xs={6}>
+                                  
                                         <FormControl variant="outlined">
                                             <InputLabel>Leave Type</InputLabel>
                                             <MuiSelect
@@ -278,15 +279,16 @@ console.log(requiredFields);
                                                 <MenuItem value="">None</MenuItem>
                                                 {
                                                     LeaveTypeData.map(
-                                                        (item, i) => (<MenuItem key={i} value={item} onClick={()=>{
+                                                        (item, i) => (<MenuItem key={i} value={item} onClick={() => {
                                                             setLeaveTypeName(item)
                                                         }} >{item}</MenuItem>)
                                                     )
                                                 }
                                             </MuiSelect>
-                                            <Typography style={{color:'red',fontSize:12,margin:3.4}}>{errors.leaveTypeName}</Typography>
+                                            <Typography style={{ color: 'red', fontSize: 12, margin: 3.4 }}>{errors.leaveTypeName}</Typography>
                                         </FormControl>
-
+                                    
+                                    
                                         <Controls.DatePicker
                                             label="Leave Start"
                                             name="startDate"
@@ -294,22 +296,11 @@ console.log(requiredFields);
                                             onChange={handleInputChange}
                                             error={errors.startDate}
                                         />
+                               
+                                </Grid>
+                                <Grid item xs={6}>
 
-                                        {/* <Controls.Select
-                                            label="Reliever"
-                                            name="relieverId"
-                                            value={values.relieverId}
-                                            options={leaveService.getEmployeeCollection()}
-                                            onChange={handleInputChange}
-                                            error={errors.relieverId}
-                                        /> */}
-
-
-
-                                    </Grid>
-                                    <Grid item xs={6}>
-
-
+                                   
                                         <Controls.Input
                                             label="Leave Duration"
                                             name="leaveDuration"
@@ -318,9 +309,9 @@ console.log(requiredFields);
                                             onChange={handleInputChange}
                                             error={errors.leaveDuration}
                                         />
+                                
 
-
-
+                                    
                                         <Controls.DatePicker
                                             label="Leave End"
                                             name="leaveEnd"
@@ -328,26 +319,23 @@ console.log(requiredFields);
                                             onChange={handleInputChange}
 
                                         />
-
-                                    </Grid>
+                                 
                                 </Grid>
-                            </Form>
+                            </Grid>
+                        </Form>
+                </div>
+            </DialogContent>
+            <DialogActions>
 
+                <Button style={{ marginRight: 350 }} variant="contained" onClick={resetForm}>Reset</Button>
+                {recordForEdit === null ? <Button variant="contained" color="primary" onClick={handleSubmit}>Add</Button> :
+                    <Button variant="contained" color="primary" onClick={updateLeaveRequest}>Update</Button>}
 
-                        </Grid>
-                    </div>
-                </DialogContent>
-                <DialogActions>
-                    
-                    <Button style={{marginRight:350}}  variant="contained" onClick={resetForm}>Reset</Button>
-                   {recordForEdit === null ? <Button variant="contained" color="primary" onClick={handleSubmit}>Add</Button> :
-                   <Button variant="contained" color="primary" onClick={updateLeaveRequest}>Update</Button>}
-                    
-                    <Button variant="contained" color="secondary" onClick={()=>{handleClose()}}>Cancel</Button>
-                   
-                </DialogActions>
+                <Button variant="contained" color="secondary" onClick={() => { handleClose() }}>Cancel</Button>
 
-            </Dialog>
-        
+            </DialogActions>
+
+        </Dialog>
+
     )
 }
